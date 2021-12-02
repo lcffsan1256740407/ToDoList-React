f01a6e5b7d9d5903e70ec4fde5a1f055b37e7472
唐洝 <.com>
2021-12-02 16:42:53 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// 引入组件
+// import Header from './component/Header'
+// import Context from './component/Context'
+// import Footer from './component/Footer'
 
+import styles from  './App.less'
+
+// 兜底的组件App
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      sad
     </div>
   );
 }
